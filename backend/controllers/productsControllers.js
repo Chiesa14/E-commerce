0833f0f6aede40d77174f937262f270684a78915
Chiesa14 <remychiesa14@gmail.com>
2024-02-09 13:30:43 +0200

    Search and other changes

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -11,23 +11,24 @@ export async function createProduct(req, res) {
 }
 export async function getAllProducts(req, res) {
   try {
-    const products = await find().sort({ createdAt: -1 });
+    const products = await Product.find().sort({ createdAt: -1 });
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json("Failed to get Products");
+    console.log(error);
   }
 }
 export async function getProduct(req, res) {
   try {
-    const product = await findById(req.params.id);
+    const product = await Product.findById(req.params.id);
     res.status(200).json(product);
   } catch (error) {
-    res.status(500).json("Failed to get product");
+    res.status(500).json(`Failed to get product with id ${req.params.id}`);
   }
 }
 export async function searchProduct(req, res) {
   try {
-    const result = await find({
+    const result = await Product.find({
       $or: [
         { title: { $regex: new RegExp(req.params.key, "i") } },
         { supplier: { $regex: new RegExp(req.params.key, "i") } },
